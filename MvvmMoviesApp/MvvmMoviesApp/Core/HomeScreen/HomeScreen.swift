df94ca4c0b8b8a2fd6e4fc355417677dc2b7a6b8
ahmethakanyldrm <65174957+ahmethakanyldrm@users.noreply.github.com>
2022-11-07 21:35:46 +0300

    Klasörleme yapıldı. Storyboard silindi.Programmatic'e uygun hale getirildi.

---
+++ MvvmMoviesApp/MvvmMoviesApp/Core/HomeScreen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  MvvmMoviesApp
+//
+//  Created by AHMET HAKAN YILDIRIM on 7.11.2022.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+      
+    }
+    
+
+
+}
---
+++ MvvmMoviesApp/MvvmMoviesApp/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  MvvmMoviesApp
+//
+//  Created by AHMET HAKAN YILDIRIM on 7.11.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = HomeScreen()
+        window?.makeKeyAndVisible()
+        
+    }
+
+
+}
+
